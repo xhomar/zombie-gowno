b7d523650ee1212028fcac5e81910c97939eccb5
xhomar <xhomar@proton.me>
2025-01-15 22:49:41 +0100

    cipce nie chcialo sie tworzyc recznie to zrobila automatem

--- scripts/js/game.js
+++ scripts/js/game.js
@@ -1,12 +1,27 @@
+function player_centre(){
+    // move player to middle of screen
+    player.css("margin-left", player_position_left);
+    player.css("margin-top", player_position_top);
+}
+function rotate(object, degree){
+    object.css("transform", "rotate(" + degree + "deg)");
+}
+
 $(document).ready(function(){
     // set variables
-    player = $("#player");
-    player_position_left = ($(window).width() - $("#player").width()) / 2;
-    player_position_top = ($(window).height() - $("#player").height()) / 2;
+    const player = $("#player");
+    let player_position_left = ($(window).width() - $("#player").width()) / 2;
+    let player_position_top = ($(window).height() - $("#player").height()) / 2;
     // positioning player
-    function player_centre(){
-        player.css("margin-left", player_position_left);
-        player.css("margin-top", player_position_top);
-    }
-    player_centre();
+    // mouse
+    $(document).mousemove(function(event) {
+        player_centre();
+        let mouse_position_top = event.pageX;
+        let mouse_position_left = event.pageY;
+        let degree = (Math.atan2((mouse_position_top - ($(window).width() / 2)), (mouse_position_left - ($(window).height()) / 2))) * (180 / Math.PI);
+        if (degree < 0) {
+            degree += 360;
+        }
+        rotate(player, -degree + 180);
+    });
   });
