18b88326b42f16e079017db7dfb1fbe41f92260a
xhomar <xhomar@proton.me>
2025-01-15 22:07:50 +0100

    cipce nie chcialo sie tworzyc recznie to zrobila automatem

--- scripts/js/game.js
+++ scripts/js/game.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
-    // jQuery methods go here...
-  
+    // set variables
+    player = $("#player");
+    player_position_left = ($(window).width() - $("#player").width()) / 2;
+    player_position_top = ($(window).height() - $("#player").height()) / 2;
+    // positioning player
+    function player_centre(){
+        player.css("margin-left", player_position_left);
+        player.css("margin-top", player_position_top);
+    }
+    player_centre();
   });
