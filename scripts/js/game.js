7919aab14346c1b4b20808d64c8a81d5b79c48f1
xhomar <xhomar@proton.me>
2025-01-15 22:51:06 +0100

    cipce nie chcialo sie tworzyc recznie to zrobila automatem

--- scripts/js/game.js
+++ scripts/js/game.js
@@ -9,15 +9,15 @@ function rotate(object, degree){
 
 $(document).ready(function(){
     // set variables
-    const player = $("#player");
-    let player_position_left = ($(window).width() - $("#player").width()) / 2;
-    let player_position_top = ($(window).height() - $("#player").height()) / 2;
+    player = $("#player");
+    player_position_left = ($(window).width() - $("#player").width()) / 2;
+    player_position_top = ($(window).height() - $("#player").height()) / 2;
     // positioning player
     // mouse
     $(document).mousemove(function(event) {
         player_centre();
-        let mouse_position_top = event.pageX;
-        let mouse_position_left = event.pageY;
+        mouse_position_top = event.pageX;
+        mouse_position_left = event.pageY;
         let degree = (Math.atan2((mouse_position_top - ($(window).width() / 2)), (mouse_position_left - ($(window).height()) / 2))) * (180 / Math.PI);
         if (degree < 0) {
             degree += 360;
