a43f642654114e1e6624bf0b58f1e457902343c2
xhomar <xhomar@proton.me>
2025-01-16 23:08:05 +0100

    player new sprite + positioning fix

--- scripts/js/player.js
+++ scripts/js/player.js
@@ -1,28 +1,41 @@
 function player_centre(){
     // move player to middle of screen
-    player_position_left = ($(window).width() - $("#player").width()) / 2;
-    player_position_top = ($(window).height() - $("#player").height()) / 2;
-    player.css("margin-left", player_position_left);
-    player.css("margin-top", player_position_top);
+
+    // calculate player's pos
+    head_position_left = ($(window).width() - head.width()) / 2;
+    head_position_top = ($(window).height() - head.height()) / 2;
+    weapon_position_left = ($(window).width() - weapon.width()) / 2;
+    weapon_position_top = ($(window).height() - weapon.height()) / 2;
+    // rotate
+    head.css("left", head_position_left);
+    head.css("top", head_position_top);
+    weapon.css("left", weapon_position_left);
+    weapon.css("top", weapon_position_top);
 }
 function rotate(object, degree){
+    // rotate object to degree
     object.css("transform", "rotate(" + degree + "deg)");
 }
 
 $(document).ready(function(){
     // set variables
-    player = $("#player");
+    head = $("#head");
+    weapon = $("#weapon");
     // positioning player
     player_centre();
     // mouse
     $(document).mousemove(function(event) {
         player_centre();
+        // get cursor pos
         mouse_position_top = event.pageX;
         mouse_position_left = event.pageY;
-        let degree = (Math.atan2((mouse_position_top - ($(window).width() / 2)), (mouse_position_left - ($(window).height()) / 2))) * (180 / Math.PI);
+        // count degree from xy(0,0) to cursor pos
+        degree = (Math.atan2((mouse_position_top - ($(window).width() / 2)), (mouse_position_left - ($(window).height()) / 2))) * (180 / Math.PI);
         if (degree < 0) {
             degree += 360;
         }
-        rotate(player, -degree + 180);
+        // rotate player
+        rotate(head, -degree);
+        rotate(weapon, -degree);
     });
   });
