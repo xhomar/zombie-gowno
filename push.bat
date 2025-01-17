e92aaddcb82e91f053a260cbe7658254048fde3f
xhomar <xhomar@proton.me>
2025-01-17 20:41:38 +0100

    Updated push.bat

--- push.bat
+++ push.bat
@@ -1,12 +1,14 @@
+@echo off
+
 :: Type 'push' to push into github with deafult commit name
 :: Type 'push "custom commit name"' to push into github with custom commit name
 
 if "%~1"=="" (
     git add .
-    git commit -m %1
+    git commit -m "pussy was too lazy to name the commit"
     git push -u origin main
 ) else (
     git add .
-    git commit -m "pussy was too lazy to name the commit"
+    git commit -m %1
     git push -u origin main
 )
