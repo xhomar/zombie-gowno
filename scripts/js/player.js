dabb851ac1e346a641102ef1fd0f2b4ee04ff521
xhomar <xhomar@proton.me>
2025-01-17 19:12:54 +0100

    cipce nie chcialo sie tworzyc recznie to zrobila automatem

--- scripts/js/player.js
+++ scripts/js/player.js
@@ -16,11 +16,17 @@ function rotate(object, degree){
     // rotate object to degree
     object.css("transform", "rotate(" + degree + "deg)");
 }
+function move(position, amount){
+    position += amount;
+    console.log(player_position.x, player_position.y)
+}
 
 $(document).ready(function(){
     // set variables
     head = $("#head");
     weapon = $("#weapon");
+    player_position = {x:0, y:0}; // PLAYER POSITION !!!!!!!!!!!!!!!!
+    key = "";
     // positioning player
     player_centre();
     // mouse
@@ -37,5 +43,19 @@ $(document).ready(function(){
         // rotate player
         rotate(head, -degree);
         rotate(weapon, -degree);
+    });
+    $(document).keydown(function (key) { 
+        if (key = "W"){
+            move(player_position.x, 1);
+        }
+        if (key = "S"){
+            move(player_position.x, -1);
+        }
+        if (key = "A"){
+            move(player_position.y, -1);
+        }
+        if (key = "D"){
+            move(player_position.y, 1);
+        }
     });
   });
