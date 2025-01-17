cca494516be56a0baffa28a4c63b24f7aae58580
xhomar <xhomar@proton.me>
2025-01-17 20:39:37 +0100

    pussy was too lazy to name the commit

--- push.bat
+++ push.bat
@@ -1,3 +1,12 @@
-git add .
-git commit -m "cipce nie chcialo sie tworzyc recznie to zrobila automatem"
-git push -u origin main
+:: Type 'push' to push into github with deafult commit name
+:: Type 'push "custom commit name"' to push into github with custom commit name
+
+if "%~1"=="" (
+    git add .
+    git commit -m %1
+    git push -u origin main
+) else (
+    git add .
+    git commit -m "pussy was too lazy to name the commit"
+    git push -u origin main
+)
