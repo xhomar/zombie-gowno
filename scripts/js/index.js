201dd693a9e149b41ec47db21b4bac6ff67b425c
M3KR4 <MarioMonsterMax@gmail.com>
2025-01-17 18:41:11 +0100

    css'a zjebałem ogólnie naprawiam

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  const settings = $("#settings");
+  const start = $("#start");
+  const buttons = $(".buttons");
+  const horizontal = $(".horizontal");
+  const popSound = document.getElementById("popSound");
 
-    // jQuery methods go here...
-  
+  popSound.volume = "0.1";
+  settings.on("click", function () {
+    // not gonna do this crap
   });
+
+  start.on("click", function () {
+    console.log("test");
+    window.location.href = "../html/game.html"; // goes to game.html
+  });
+
+  buttons.on("mouseover", function () {
+    popSound.currentTime = 0; // Restarts the sound
+    popSound.play(); // Plays the sound
+  });
+});
