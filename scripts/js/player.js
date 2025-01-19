7738603e737aa7e0f9642db2854755f0c92b195c
xhomar <xhomar@proton.me>
2025-01-19 18:05:44 +0100

    loop lagging fix, created zombie.js, small code improvements

--- scripts/js/player.js
+++ scripts/js/player.js
@@ -2,23 +2,26 @@
 IMPORTANT!!!!
 
 use pull before you code anything!!!! -fuck you
-*/ 
+*/
 
-$(document).ready(function(){
+$(document).ready(function () {
     // set variables
     key_down_w = false, key_down_a = false, key_down_s = false, key_down_d = false;
     head = $("#head");
     weapon = $("#weapon");
-    player_position = {x:0, y:0}; // PLAYER POSITION !!!!!!!!!!!!!!!!
+    window.player_position = { x: 0, y: 0 }; // PLAYER POSITION !!!!!!!!!!!!!!!!
+    mouse_position = { x: 0, y: 0 };
+    window.head_position = { x: 0, y: 0 };
+    weapon_position = { x: 0, y: 0 };
     // positioning player
     loop();
     // mouse
-    $(document).mousemove(function(event) {
+    $(document).mousemove(function (event) {
         // get cursor pos
-        mouse_position_top = event.pageX;
-        mouse_position_left = event.pageY;
+        mouse_position.y = event.pageX;
+        mouse_position.x = event.pageY;
         // count degree from xy(0,0) to cursor pos
-        degree = (Math.atan2((mouse_position_top - ($(window).width() / 2)), (mouse_position_left - ($(window).height()) / 2))) * (180 / Math.PI);
+        degree = (Math.atan2((mouse_position.y - ($(window).width() / 2)), (mouse_position.x - ($(window).height()) / 2))) * (180 / Math.PI);
         if (degree < 0) {
             degree += 360;
         }
@@ -26,10 +29,26 @@ $(document).ready(function(){
         rotate(head, -degree);
         rotate(weapon, -degree);
     });
+    function player_centre() {
+        // move player to middle of screen
+    
+        // calculate player's pos
+        window.head_position.x = ($(window).width() - head.width()) / 2;
+        window.head_position.y = ($(window).height() - head.height()) / 2;
+        weapon_position.x = ($(window).width() - weapon.width()) / 2;
+        weapon_position.y = ($(window).height() - weapon.height()) / 2;
+        // rotate
+        head.css({"left": window.head_position.x, "top": window.head_position.y});
+        weapon.css({"left": weapon_position.x, "top": weapon_position.y});
+    }
+    function rotate(object, degree) {
+        // rotate object to degree
+        object.css("transform", "rotate(" + degree + "deg)");
+    }
     // key down
-    $(document).keydown(function (event) { 
+    $(document).keydown(function (event) {
         // checks if keys are hold down
-        switch(event.key){
+        switch (event.key) {
             case "w":
                 key_down_w = true;
                 break;
@@ -45,9 +64,9 @@ $(document).ready(function(){
         }
     });
     // key up
-    $(document).keyup(function (event) { 
+    $(document).keyup(function (event) {
         // checks if keys are not longer hold down
-        switch(event.key){
+        switch (event.key) {
             case 'w':
                 key_down_w = false;
                 break;
@@ -66,41 +85,26 @@ $(document).ready(function(){
         // moving player to screen's centre
         player_centre();
         // player movement
-        if (key_down_w){
-            player_position.x += 1; //x+1
-            console.log(player_position);
+        if (key_down_w) {
+            window.player_position.x += 1; //x+1
+            console.clear();
+            console.log(window.player_position);
         };
-        if (key_down_a){
-            player_position.y -= 1; //y-1
-            console.log(player_position);
+        if (key_down_a) {
+            window.player_position.y -= 1; //y-1
+            console.clear();
+            console.log(window.player_position);
         };
-        if (key_down_s){
-            player_position.x -= 1; //x-1
-            console.log(player_position);
+        if (key_down_s) {
+            window.player_position.x -= 1; //x-1
+            console.clear();
+            console.log(window.player_position);
         };
-        if (key_down_d){
-            player_position.y += 1; //y+1
-            console.log(player_position);
+        if (key_down_d) {
+            window.player_position.y += 1; //y+1
+            console.clear();
+            console.log(window.player_position);
         };
         requestAnimationFrame(loop);
-    }
-  });
-
-function player_centre(){
-    // move player to middle of screen
-
-    // calculate player's pos
-    head_position_left = ($(window).width() - head.width()) / 2;
-    head_position_top = ($(window).height() - head.height()) / 2;
-    weapon_position_left = ($(window).width() - weapon.width()) / 2;
-    weapon_position_top = ($(window).height() - weapon.height()) / 2;
-    // rotate
-    head.css("left", head_position_left);
-    head.css("top", head_position_top);
-    weapon.css("left", weapon_position_left);
-    weapon.css("top", weapon_position_top);
-}
-function rotate(object, degree){
-    // rotate object to degree
-    object.css("transform", "rotate(" + degree + "deg)");
-}
+    };
+});
---
+++ scripts/js/zombie.js
@@ -0,0 +1,14 @@
+class ZombieIndex {
+    constructor(){
+        this.head = "";
+        this.weapon = "";
+        this.position = { x: 0, y: 0 };
+    }
+}
+
+$(document).ready(function () {
+    console.log(window.player_position);
+    head = $("#head");
+    weapon = $("#weapon");
+    position = { x: 0, y: 0 };
+});
